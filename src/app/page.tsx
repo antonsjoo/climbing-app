634e07878983780e13a8ed7a036517f5c65151e5
Anton Sjöö <anton.sjoo@hotmail.se>
2024-04-28 19:43:12 +0200

    Added shadcn deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello. This IS the app!
+      <h1 className="font-bold text-3xl text-center">
+        Wow what a nice button.
+      </h1>
+      <Button variant="outline">Button</Button>
     </main>
   );
 }
